ac59ad8fd1879bc521b29a796869d4d8077727b9
zhengjunling <zhengjunling@didichuxing.com>
2019-05-05 17:31:47 +0800

    scss替换成less

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import './scss/main.scss'
+import './less/main.less'
 
 Vue.use(ElementUI)
 
