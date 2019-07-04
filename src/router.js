959337c752d3717217d588f2e73c2597f40fcd00
zhengjunling <zhengjunling@didichuxing.com>
2019-07-04 10:49:12 +0800

    更新

--- src/router.js
+++ src/router.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from './views/Login'
+import Main from './views/Main'
 import Home from './views/home'
 import Setting from './views/Setting'
 import Sms from './views/sms'
@@ -24,148 +26,159 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
       path: '/',
-      name: 'home',
-      component: Home,
-      meta: {
-        title: '首页',
-        name: Home.name
-      }
-    },
-    {
-      path: '/setting',
-      name: 'setting',
-      component: Setting,
-      meta: {
-        title: '账号设置',
-        name: Setting.name
-      }
-    },
-    {
-      path: '/sms',
-      name: 'sms',
-      component: Sms,
-      meta: {
-        title: '短信发送',
-        name: Sms.name
-      }
-    },
-    {
-      path: '/dynamicSms',
-      name: 'dynamicSms',
-      component: DynamicSms,
-      meta: {
-        title: '动态短信',
-        name: DynamicSms.name
-      }
-    },
-    {
-      path: '/template',
-      name: 'template',
-      component: Template,
-      meta: {
-        title: '常用短语',
-        name: Template.name
-      }
-    },
-    {
-      path: '/immediateTasks',
-      name: 'immediateTasks',
-      component: ImmediateTasks,
-      meta: {
-        title: '即时任务列表',
-        name: ImmediateTasks.name
-      }
-    },
-    {
-      path: '/appointmentTasks',
-      name: 'appointmentTasks',
-      component: AppointmentTasks,
-      meta: {
-        title: '预约任务列表',
-        name: AppointmentTasks.name
-      }
-    },
-    {
-      path: '/fileSendHistory',
-      name: 'fileSendHistory',
-      component: FileSendHistory,
-      meta: {
-        title: '文件发送历史记录',
-        name: FileSendHistory.name
-      }
-    },
-    {
-      path: '/demand',
-      name: 'demand',
-      component: Demand,
-      meta: {
-        title: '点播服务',
-        name: Demand.name
-      }
-    },
-    {
-      path: '/smsReply',
-      name: 'smsReply',
-      component: SmsReply,
-      meta: {
-        title: '短信回复',
-        name: SmsReply.name
-      }
-    },
-    {
-      path: '/dailyStatistics',
-      name: 'dailyStatistics',
-      component: DailyStatistics,
-      meta: {
-        title: '日发送统计',
-        name: DailyStatistics.name
-      }
-    },
-    {
-      path: '/monthlyStatistics',
-      name: 'monthlyStatistics',
-      component: MonthlyStatistics,
-      meta: {
-        title: '月发送统计',
-        name: MonthlyStatistics.name
-      }
-    },
-    {
-      path: '/statusQuery',
-      name: 'statusQuery',
-      component: StatusQuery,
-      meta: {
-        title: '状态查询',
-        name: StatusQuery.name
-      }
-    },
-    {
-      path: '/contacts',
-      name: 'contacts',
-      component: Contacts,
-      meta: {
-        title: '客户通讯录',
-        name: Contacts.name
-      }
-    },
-    {
-      path: '/blacklist',
-      name: 'blacklist',
-      component: BlackList,
-      meta: {
-        title: '黑名单管理',
-        name: BlackList.name
-      }
-    },
-    {
-      path: '/download',
-      name: 'download',
-      component: Download,
-      meta: {
-        title: '下载中心',
-        name: Download.name
-      }
+      component: Main,
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: Home,
+          meta: {
+            title: '首页',
+            name: Home.name
+          }
+        },
+        {
+          path: '/setting',
+          name: 'setting',
+          component: Setting,
+          meta: {
+            title: '账号设置',
+            name: Setting.name
+          }
+        },
+        {
+          path: '/sms',
+          name: 'sms',
+          component: Sms,
+          meta: {
+            title: '短信发送',
+            name: Sms.name
+          }
+        },
+        {
+          path: '/dynamicSms',
+          name: 'dynamicSms',
+          component: DynamicSms,
+          meta: {
+            title: '动态短信',
+            name: DynamicSms.name
+          }
+        },
+        {
+          path: '/template',
+          name: 'template',
+          component: Template,
+          meta: {
+            title: '常用短语',
+            name: Template.name
+          }
+        },
+        {
+          path: '/immediateTasks',
+          name: 'immediateTasks',
+          component: ImmediateTasks,
+          meta: {
+            title: '即时任务列表',
+            name: ImmediateTasks.name
+          }
+        },
+        {
+          path: '/appointmentTasks',
+          name: 'appointmentTasks',
+          component: AppointmentTasks,
+          meta: {
+            title: '预约任务列表',
+            name: AppointmentTasks.name
+          }
+        },
+        {
+          path: '/fileSendHistory',
+          name: 'fileSendHistory',
+          component: FileSendHistory,
+          meta: {
+            title: '文件发送历史记录',
+            name: FileSendHistory.name
+          }
+        },
+        {
+          path: '/demand',
+          name: 'demand',
+          component: Demand,
+          meta: {
+            title: '点播服务',
+            name: Demand.name
+          }
+        },
+        {
+          path: '/smsReply',
+          name: 'smsReply',
+          component: SmsReply,
+          meta: {
+            title: '短信回复',
+            name: SmsReply.name
+          }
+        },
+        {
+          path: '/dailyStatistics',
+          name: 'dailyStatistics',
+          component: DailyStatistics,
+          meta: {
+            title: '日发送统计',
+            name: DailyStatistics.name
+          }
+        },
+        {
+          path: '/monthlyStatistics',
+          name: 'monthlyStatistics',
+          component: MonthlyStatistics,
+          meta: {
+            title: '月发送统计',
+            name: MonthlyStatistics.name
+          }
+        },
+        {
+          path: '/statusQuery',
+          name: 'statusQuery',
+          component: StatusQuery,
+          meta: {
+            title: '状态查询',
+            name: StatusQuery.name
+          }
+        },
+        {
+          path: '/contacts',
+          name: 'contacts',
+          component: Contacts,
+          meta: {
+            title: '客户通讯录',
+            name: Contacts.name
+          }
+        },
+        {
+          path: '/blacklist',
+          name: 'blacklist',
+          component: BlackList,
+          meta: {
+            title: '黑名单管理',
+            name: BlackList.name
+          }
+        },
+        {
+          path: '/download',
+          name: 'download',
+          component: Download,
+          meta: {
+            title: '下载中心',
+            name: Download.name
+          }
+        }
+      ]
     }
   ],
   scrollBehavior(to, from, savedPosition) {
---
+++ src/views/Main.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="main-container">
+    <header-bar></header-bar>
+    <menu-bar></menu-bar>
+    <el-tabs
+      class="header-tabs"
+      v-model="active"
+      type="card"
+      :closable="tabs.length > 1"
+      @tab-remove="removeTab"
+      @tab-click="handleTabChange"
+    >
+      <el-tab-pane v-for="item in tabs" :key="item.name" :label="item.title" :name="item.name"></el-tab-pane>
+    </el-tabs>
+    <div class="view-container">
+      <keep-alive :include="keepAlives">
+        <router-view></router-view>
+      </keep-alive>
+    </div>
+  </div>
+</template>
+
+<script>
+import HeaderBar from '@/components/HeaderBar'
+import MenuBar from '@/components/MenuBar'
+export default {
+  components: {
+    HeaderBar,
+    MenuBar
+  },
+
+  data() {
+    return {
+      tabs: [],
+      keepAliveTabs: [],
+      active: null
+    }
+  },
+
+  computed: {
+    keepAlives() {
+      return this.tabs.map(tab => tab.viewName)
+    }
+  },
+
+  watch: {
+    $route: 'updateTabs'
+  },
+
+  created() {
+    this.$store.dispatch('fetchUserInfo')
+    this.updateTabs()
+  },
+
+  methods: {
+    updateTabs() {
+      const { path, meta } = this.$route
+      const tab = this.tabs.find(tab => tab.name === path)
+
+      if (!tab) {
+        this.tabs.push({
+          name: path,
+          title: meta.title,
+          viewName: meta.name
+        })
+      }
+
+      // tab做了多开限制，最多只允许开8个选项卡，超出的话将按照打开顺序先后移除
+      // if (this.tabs.length > 8) {
+      //   this.tabs.shift();
+      // }
+
+      this.active = path
+    },
+
+    handleTabChange() {
+      this.$router.push({
+        path: this.active
+      })
+    },
+
+    /**
+     * 删除tab
+     * @param name 将要删除的tab标识
+     */
+    removeTab(name) {
+      const index = this.tabs.findIndex(tab => tab.name === name)
+      const nextIndex = index < this.tabs.length - 1 ? index + 1 : index - 1
+      this.$router.replace({
+        path: this.tabs[nextIndex].name
+      })
+      this.tabs = this.tabs.filter(tab => tab.name !== name)
+    }
+  }
+}
+</script>
